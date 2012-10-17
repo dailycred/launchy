6b2dde08e6d2770d9df91d52a9bf8904a733c2a7
Hank Stoever <hstove@gmail.com>
2012-10-17 09:58:45 -0700

    removed bad migration file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.configuration.DAILYCRED_CLIENT_ID = ENV['launchy_dc_id'] || "YOUR-CLIENT-ID"
-p ENV['launchy_dc_id']
 Rails.configuration.DAILYCRED_SECRET_KEY = ENV['launchy_dc_secret'] || "YOUR-CLIENT-SECRET"
 
 #this is used to redirect to '/thanks' after login and '/' after logout
--- db/migrate/20121010195618_remove_limit_from_created.rb
+++
@@ -1,8 +0,0 @@
-class RemoveLimitFromCreated < ActiveRecord::Migration
-  def up
-    change_column :users, :created, :integer, :limit => 20
-  end
-
-  def down
-  end
-end
