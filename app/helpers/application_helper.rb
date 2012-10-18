a50d6e8c2c16c25b0b077d6309c934e523d1c3db
Hank Stoever <hstove@gmail.com>
2012-10-17 17:25:08 -0700

    much better social sharing links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,32 @@ module ApplicationHelper
   def current_url
     "#{request.protocol}#{request.host_with_port}#{request.fullpath}"
   end
+
+  def tweet_url opts={}
+    opts[:url] ||= current_url
+    url = "http://twitter.com/share"
+    make_url url, opts
+  end
+
+  def facebook_url opts={}
+    opts[:u] ||= current_url
+    make_url "http://facebook.com/sharer/sharer.php", opts
+  end
+
+  def gplus_url opts={}
+    opts[:url] ||= current_url
+    make_url "https://plus.google.com/share", opts
+  end
+
+  private
+
+  def make_url url, opts={}
+    url += "?"
+    params = []
+    opts.each do |k,v|
+      params.push "#{k}=#{u(v)}"
+    end
+    url += params.join("&")
+  end
+
 end
